5f0252da2c3911ff0cdb55f1a394a038ac1e5612
Gabriel Cousin <csngab@gmail.com>
2017-09-16 18:22:38 +0200

    Simplify App.js

--- src/components/App.js
+++ src/components/App.js
@@ -15,48 +15,22 @@ import {
 } from 'react-router-dom'
 
 class App extends Component {
-  connectWrapper = () => {
-    const isAuthenticatedOnSpotify = Boolean(localStorage.getItem('spotify_access_token'))
-    return (
-      <div>
-        <DiscogsConnect />
-        <SpotifyConnect isAuthenticated={isAuthenticatedOnSpotify} />
-      </div>
-    )
-  }
-
-  releasesListWrapper = () => (
-    <ReleasesList />
-  )
-
-  discogsRedirect = () => (
-    <DiscogsCallback />
-  )
-
-  spotifyRedirect = () => (
-    <SpotifyCallback />
-  )
-
-  logoutWrapper = () => (
-    <Logout />
-  )
-
   render() {
     return (
       <Router>
         <div className="App">
           <h1>Discotify</h1>
-          <p>Export your Discogs collection to your Spotify library</p>
-            <div>
-              <Route exact path="/logout" component={this.logoutWrapper} />
-              <Route exact path="/discogs_callback" component={this.discogsRedirect} />
-              <Route exact path="/spotify_callback" component={this.spotifyRedirect} />
-              <Route exact path="/" component={this.connectWrapper} />
-              <Route exact path="/" component={this.releasesListWrapper} />
-            </div>
-            <a target="_black" href="https://www.discogs.com/settings/applications">Manage Discogs Access</a>
-            <span> - </span>
-            <Link to="/logout">Log out</Link>
+          <div>
+            <Route exact path="/logout" component={Logout} />
+            <Route exact path="/discogs_callback" component={DiscogsCallback} />
+            <Route exact path="/spotify_callback" component={SpotifyCallback} />
+            <Route exact path="/" component={DiscogsConnect} />
+            <Route exact path="/" component={SpotifyConnect} />
+            <Route exact path="/" component={ReleasesList} />
+          </div>
+          <a target="_black" href="https://www.discogs.com/settings/applications">Manage Discogs Access</a>
+          <span> - </span>
+          <Link to="/logout">Log out</Link>
         </div>
       </Router>
     );
