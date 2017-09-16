8d0f99947b414c5d1e691483e309a67284210f25
Gabriel Cousin <csngab@gmail.com>
2017-09-16 18:23:03 +0200

    Config | Add link to Spotify dev website

--- src/config/settings.dist.js
+++ src/config/settings.dist.js
@@ -3,6 +3,7 @@ export const DISCOGS_CONSUMER_KEY = 'DISCOGS_CONSUMER_KEY'
 export const DISCOGS_CONSUMER_SECRET = 'DISCOGS_CONSUMER_SECRET'
 export const DISCOGS_OAUTH_CALLBACK = encodeURIComponent('http://localhost:3000/discogs_callback')
 
+// Create yours on https://developer.spotify.com/my-applications
 export const SPOTIFY_CLIENT_ID = 'SPOTIFY_CLIENT_ID'
 export const SPOTIFY_CLIENT_SECRET = 'SPOTIFY_CLIENT_SECRET'
 export const SPOTIFY_OAUTH_CALLBACK = encodeURIComponent('http://localhost:3000/spotify_callback')
