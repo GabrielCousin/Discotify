b0e14f124ea6e9bdf251d4421d9d8350a459fbb2
Gabriel Cousin <csngab@gmail.com>
2017-12-03 15:09:53 +0100

    Use React.Fragment

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const Fragment = React.Fragment;
+
 function Splash () {
   return (
-    <div>
+    <Fragment>
       <h2>On-site? No take away!</h2>
       <p>For Discogs users who don’t listen to music only in their house but also outside.</p>
       <p>Export your Discogs collection to Spotify (moar coming soon!)</p>
       <Link to='/match'>Begin</Link>
-    </div>
+    </Fragment>
   )
 }
 
