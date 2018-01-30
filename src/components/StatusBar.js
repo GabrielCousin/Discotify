0d147c77745f4a72e11726f82f185ed74cb09145
Gabriel Cousin <csngab@gmail.com>
2018-01-30 21:22:45 +0100

    Improve StatusBar

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -13,7 +13,7 @@ const mapStateToProps = (state) => {
 class StatusBar extends Component {
   constructor(props) {
     super(props);
-    this.handleSpotifyExport = this.handleSpotifyExport.bind(this);
+    this.handleSpotifyExport = this.handleSpotifyExport.bind(this)
   }
 
   handleSpotifyExport () {
@@ -25,16 +25,30 @@ class StatusBar extends Component {
     let ignoredReleases = this.props.releases.filter((rlz) => (rlz.status === 'fail')).length
 
     return (
-      <div className={processedReleases ? 'Box' : 'Box Box--hidden' }>
+      <div className={processedReleases ? 'Box' : 'Box Box--hidden'}>
         <div className="Box-Content">
           <div><strong>{processedReleases}</strong> on <strong>{this.props.releases.length}</strong> releases processed.</div>
           <div><strong>{ignoredReleases} items</strong> will be ignored.</div>
         </div>
-        <div className="Box-AddOn">
-          <progress value={processedReleases} max={this.props.releases.length} />
-          <button className="Button" type="button" onClick={this.handleSpotifyExport} disabled={!this.props.app.release_matching_done}>Export to Spotify</button>
-          <a className="Button" href="https://open.spotify.com/collection/albums" target="_blank" rel="noopener noreferrer">All done! Go to Spotify</a>
-        </div>
+        {!this.props.app.release_matching_done &&
+          <progress className='ProgressBar' value={processedReleases} max={this.props.releases.length} />
+        }
+        {this.props.app.release_matching_done &&
+          <div className="Box-AddOn">
+            <button
+              className="Button"
+              type="button"
+              onClick={this.handleSpotifyExport}
+              disabled={this.props.app.release_export_done}
+            >Export to Spotify</button>
+            <a
+              className={`Button ${this.props.app.release_export_done ? '' : 'Button--disabled'}`}
+              href="https://open.spotify.com/collection/albums"
+              target="_blank"
+              rel="noopener noreferrer"
+            >All done! Go to Spotify</a>
+          </div>
+        }
       </div>
     );
   }
