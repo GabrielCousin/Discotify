88773ff0b5c6772449594a470006ee10b9af7057
AlexandreBroudin <broudin.alexandre@gmail.com>
2018-03-28 14:11:36 +0200

    Footer | Add credits 💋

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Splash () {
   return (
     <footer className="Footer">
-      Öpücük 2018 💋 <a className="Link" href="https://twitter.com/abroudin">Contact</a> 💋
+      Made by <a className="Link" href="https://twitter.com/GabrielCousin">Gabriel Cousin</a> & <a className="Link" href="https://twitter.com/abroudin">Alexandre Broudin</a> 💋 Öpücük 2018 💋 <a className="Link" href="https://twitter.com/abroudin">Contact</a> 💋
       <Link  className="Link" to="/logout">Log out</Link>
     </footer>
   )
