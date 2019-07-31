33c5bee32b46fdd6f678adb0ca0ed10435cc5874
Gabriel Cousin <7649529+GabrielCousin@users.noreply.github.com>
2019-07-31 13:50:15 +0200

    Footer | Update year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Splash () {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="Footer">
-      Made by <a className="Link" href="https://twitter.com/GabrielCousin">Gabriel Cousin</a> & <a className="Link" href="https://twitter.com/abroudin">Alexandre Broudin</a> 💋 Öpücük 2018 💋 <a className="Link" href="https://twitter.com/abroudin">Contact</a> 💋
+      Made by <a className="Link" href="https://twitter.com/GabrielCousin">Gabriel Cousin</a> & <a className="Link" href="https://twitter.com/abroudin">Alexandre Broudin</a> 💋 Öpücük {currentYear} 💋 <a className="Link" href="https://twitter.com/abroudin">Contact</a> 💋
       <Link  className="Link" to="/logout">Log out</Link>
     </footer>
   )
